b8472d8345ef56f547849308432fdefee4927447
Vladimir Tazin <vladimir@wildapricot.com>
2017-09-05 13:53:23 +0300

    WA-31634 Add basic implementation

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # wa-consul-kv-sync
+
+## Installation
+
+```
+npm i wa-consul-kv-sync
+```
+
+## Usage
+
+```
+wa-consul-kv-sync [options] [path-to-config-root] [path-to-output-file]
+```
+
+### path-to-config-root
+```
+    Path to configuration root.
+```
+
+### path-to-output-file
+```
+    Path to output file.
+```
+
+### options:
+
+```
+    -h, --help         output usage information
+    -V, --version      output the version number
+```
+
+### Expamples:
+
+```
+    $ wa-consul-kv-sync /home/wa/consul/config /home/wa/config/consul.json
+```
+
+To view help:
+```
+    $ wa-consul-kv-sync -h
+```
+
+To show current version:
+```
+    $ wa-consul-kv-sync -V
+```
--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-console.log('gotcha!');
+#!/usr/bin/env node
+
+const commander = require('commander');
+const fs = require('fs');
+const pkg = require('./package.json');
+const getDirFiles = require('./lib/get-dir-files');
+const errorHandler = require('./lib/error-handler');
+const addErrorHandler = require('./lib/add-error-handler');
+const processFileData = require('./lib/process-file-data');
+const getFileKey = require('./lib/get-file-key');
+const createOnAllWritten = require('./lib/on-all-written');
+
+const onHelp = () =>
+{
+  console.log('    Examples:');
+  console.log('');
+  console.log('    $ wa-consul-kv-sync /home/wa/consul/config /home/wa/config/consul.json');
+  console.log('');
+  console.log('    $ wa-consul-kv-sync -h');
+  console.log('');
+  console.log('    $ wa-consul-kv-sync -V');
+  console.log('');
+};
+
+commander.version(pkg.version)
+  .usage('[config path]')
+  .description('Synchronizes one or more JSON manifests with consul\'s key value store.')
+  .on('--help', onHelp);
+
+commander.parse(process.argv);
+
+if (!commander.args.length || commander.args.length !== 2)
+{
+  commander.outputHelp();
+  process.exit(1);
+}
+
+const configRoot = commander.args[0];
+const output = commander.args[1];
+
+const files = getDirFiles(configRoot, errorHandler);
+let writes = 0;
+
+const onComplete = () =>
+{
+  console.log('Complete. ' + files.length + ' files processed.');
+  process.exit(0);
+};
+
+const createOnDataWritten = (file, filesLength, onAllWritten) => addErrorHandler(() =>
+{
+  writes++;
+  console.log('"' + file + '" processed, ' + writes + ' of ' + filesLength);
+
+  if (writes === filesLength)
+  {
+    onAllWritten();
+  }
+});
+
+fs.writeFile(output, '{', 'utf8', addErrorHandler(() =>
+{
+  const onAllWritten = createOnAllWritten(output, onComplete);
+
+  files.forEach(file =>
+  {
+    const onDataWritten = createOnDataWritten(file, files.length, onAllWritten);
+    const onFileRead = processFileData(output, getFileKey(file, configRoot.length), onDataWritten);
+
+    fs.readFile(file, 'utf8', onFileRead);
+  });
+}));
+
